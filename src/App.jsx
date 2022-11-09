4ecaa65d1c6fc5248c577fd9c0c87fcef56e4a7a
GaryNow <you@example.com>
2022-11-09 06:19:29 -0500

    v1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './assets/imgs/logo.png'
+
+function App() {
+
+  return (
+    <div className="aplicacion-tareas">
+        <div className="freecodecamp-logo-contenedor">
+          <img src={logo} alt="freecodecamp-logo" />
+
+        </div>
+    </div>
+  )
+}
+
+export default App
